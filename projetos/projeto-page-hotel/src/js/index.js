8864342dca5466482919b89528ed23f6c93776aa
Victor Hugo <108891666+Victor-Hugo-inacio@users.noreply.github.com>
2022-08-01 16:43:33 -0300

    Javascript do menu hamburguer do responsivo

--- projetos/projeto-page-hotel/src/js/index.js
+++ projetos/projeto-page-hotel/src/js/index.js
@@ -1,1 +1,9 @@
-
+const menuHamburguer = document.getElementById("menu");
+const opcao = document.querySelectorAll(".opcoes");
+let click = 0;
+menuHamburguer.addEventListener("click", function() {
+  opcao[0].classList.add("active");
+  click++;
+  if (click > 1) click = 0;
+  if (click == 0) opcao[0].classList.remove("active");
+});
