3c1f52ae51a212c4ddd2df1a8e2978b36e59e1fb
Victor Hugo <108891666+Victor-Hugo-inacio@users.noreply.github.com>
2022-08-01 19:40:42 -0300

    Js do projeto e finalização

--- projetos/projeto-slider-mapadev-week/src/js/index.js
+++ projetos/projeto-slider-mapadev-week/src/js/index.js
@@ -1,1 +1,30 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
 
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao) {
+  cartoes[indiceCartao].classList.add("selecionado");
+}
+
+let cartaoAtual = 0;
+
+btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
+  
+  esconderCartaoSelecionado();
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
+  
+  esconderCartaoSelecionado();
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
