96174d6666be0e3c5c273dac1651772e25389a3f
Victor Hugo <108891666+Victor-Hugo-inacio@users.noreply.github.com>
2022-08-07 17:36:43 -0300

    Arquivo pra adicionar e retirar uma classe pro menu hamburguer

--- land-page-agencia/src/js/script.js
+++ land-page-agencia/src/js/script.js
@@ -1,1 +1,13 @@
+const checkbox = document.getElementById('checkbox');
 
+checkbox.addEventListener('click', function() {
+  
+  const menu = document.querySelector('div.menu');
+  
+  if (!checkbox.checked) {
+    menu.classList.remove("active");
+  } else {
+    menu.classList.add("active");
+  }
+  
+});
